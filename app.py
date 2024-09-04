c0ee189f1aa4eca7db96384b5d41a6396381a9d4
akaguiz <56702825+akaguiz@users.noreply.github.com>
2024-09-03 22:55:23 -0700

    shell error

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config('SQLALCHEMY_DATABASE_URI') = 'sqlite:///ecommerce.db'
+
+db = SQLAlchemy(app)
+
+# Product (id, name, price, description)
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+
+# Routes
+@app.route('/')
+def hello_world(): 
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
